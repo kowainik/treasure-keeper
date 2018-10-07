fc3371671d6fcfe53b564b82c6f553889d8d437b
Dmitrii Kovanikov <kovanikov@gmail.com>
2018-10-07 17:22:58 +0800

    [#4] Introduce Record data type (#7)
    * [#4] Introduce Record data type
    
    * Allow amount be negative
    
    * Fix stack CI

---
+++ src/Treasure/Core/Record.hs
@@ -0,0 +1,37 @@
+module Treasure.Core.Record
+       ( Record (..)
+
+       , Tag (..)
+       , mkTag
+
+       , Amount (..)
+       ) where
+
+import qualified Data.Text as T
+
+{- | Expenses record.
+-}
+data Record = Record
+    { -- | Account from what we're spending money
+      recordAccountFrom :: Undefined
+
+      -- | Tags for the spent amount of money.
+    , recordTags        :: NonEmpty Tag
+
+      -- | Amount of spent many.
+    , recordAmount      :: Amount
+    }
+
+-- | Expense record tag. Can be used for search.
+newtype Tag = Tag
+    { unTag :: Text
+    }
+
+mkTag :: Text -> Maybe Tag
+mkTag tag
+    | T.null tag = Nothing
+    | otherwise  = Just (Tag tag)
+
+newtype Amount = Amount
+    { unAmount :: Rational
+    } deriving newtype (Eq, Ord, Num)
